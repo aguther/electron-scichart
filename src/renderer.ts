219e4fd5377e76f4a87596c4f0ca961a87061f60
Andreas Guther <andreas@guther.family>
2024-02-05 21:22:53 +0100

    added SciChart and made it working

--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,7 @@
  * ```
  */
 
+import "./chartInit2D";
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -1,10 +1,18 @@
-import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/default
+import CopyPlugin from "copy-webpack-plugin";
 
 export const plugins = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
+    new ForkTsCheckerWebpackPlugin({
+        logger: 'webpack-infrastructure',
+    }),
+    new CopyPlugin({
+        patterns: [
+            {from: "node_modules/scichart/_wasm/scichart2d.wasm", to: "main_window"},
+            {from: "node_modules/scichart/_wasm/scichart2d.data", to: ""},
+            {from: "node_modules/scichart/_wasm/scichart3d.wasm", to: "main_window"},
+            {from: "node_modules/scichart/_wasm/scichart3d.data", to: ""},
+        ],
+    }),
 ];
